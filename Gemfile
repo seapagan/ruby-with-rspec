b6c5e96437b1899e4448f7b5909907d8d17fa9b7
Grant Ramsay <seapagan@gmail.com>
2022-04-05 08:45:50 +0000

    end of Section 3

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'reek', '~> 6.1'
+gem 'rspec', '~> 3.11'
+gem 'rubocop', '~> 1.26'
+gem 'rubocop-rspec', '~> 2.9'
---
+++ spec/respond_to_matcher_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class HotChocolate
+  def drink
+    'Delicious'
+  end
+
+  def discard
+    'PLOP!'
+  end
+
+  def purchase(number)
+    "Awesome, I just purchased #{number} more hot chocolate"
+  end
+end
+
+RSpec.describe HotChocolate do
+  it 'confirms that an object can respond to a method' do
+    expect(subject).to respond_to :drink
+    expect(subject).to respond_to :drink, :discard
+    expect(subject).to respond_to :drink, :discard, :purchase
+  end
+
+  it 'confirms that an object can respond to a method with arguments' do
+    expect(subject).to respond_to :purchase
+    expect(subject).to respond_to(:purchase).with(1).arguments
+  end
+
+  it { is_expected.to respond_to :purchase, :discard }
+  it { is_expected.to respond_to(:purchase).with(1).arguments }
+end
